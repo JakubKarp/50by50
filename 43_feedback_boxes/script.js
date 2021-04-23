aff81e3903e61127a3f5eade197fdfe95235ee76
j.karp <j.karp@createit.com>
2021-04-23 14:04:09 +0200

    43feedback-boxes

---
+++ 43_feedback_boxes/script.js
@@ -0,0 +1,31 @@
+const ratings = document.querySelectorAll('.rating')
+const ratingsContainer = document.querySelector('.rating-container')
+const sendBtn = document.querySelector('#send')
+const panel = document.querySelector('#panel')
+let selectedRating = 'Satisfied'
+
+ratingsContainer.addEventListener('click', (e) => {
+  // we use bubbling
+  if(e.target.parentNode.classList.contains('rating')) {
+    removeActive()
+    e.target.parentNode.classList.add('active')
+    selectedRating = e.target.nextElementSibling.innerHTML
+    console.log('panel.addEventListener / selectedRating', selectedRating)
+  }
+})
+
+sendBtn.addEventListener('click', () => {
+  panel.innerHTML = `
+    <i class="fas fa-heart"></i>
+    <strong>Thank You!</strong>
+    <br>
+    <strong>Feedback: ${selectedRating}</strong>
+    <p>We'll use your feedback to improve our customer support</p>
+  `
+})
+
+function removeActive() {
+  ratings.forEach(item => {
+    item.classList.remove('active')
+  })
+}
